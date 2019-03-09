b5706756afb8a9b1d1a4f45a5afe1640c372eeb1
Adrián Camilo Cortés V <42278980+V4L3NC1IA@users.noreply.github.com>
2019-03-09 13:07:03 -0500

    Los primeros constructores con sus setteres y getters, ademas se añado la primera parte del UML

---
+++ src/model/Client.java
@@ -0,0 +1,41 @@
+package model;
+public class Client{
+	//Attributes
+	private String name;
+	private int id;
+	private int address;
+	private int telephoneContac;
+
+	//method main
+	public Client(String theName,int theId, int theAddress, int ThetelephoneContac){
+		name = theName;
+		id = theId;
+		address = theAddress;
+		telephoneContac = ThetelephoneContac; 
+	}
+	//Setters and Getters
+	public String getName (){
+		return name;
+	}
+	public void setName(String name){
+		this.name= name;
+	}
+	public int getId(){
+		return id;
+	}
+	public void setId(int id){
+		this.id=id;
+	}
+	public int getAddress(){
+		return address;
+	}
+	public void setAddress(int address){
+		this.address= address;
+	}
+	public int getTelephoneContac(){
+		return telephoneContac;
+	}
+	public void setTelephoneContac(int telephoneContac){
+		this.telephoneContac=telephoneContac;
+	}
+}
---
+++ src/model/Pet.java
@@ -0,0 +1,42 @@
+package model;
+public class Pet{
+	//attributes
+	private String name;
+	private String typeOfPet;
+	private int age;
+	private double weight;
+	
+	//Method main
+	public Pet(String theName,String theTypeOfPet, int theAge, double theWeight){
+		name= theName;
+		typeOfPet= theTypeOfPet;
+		age= theAge;
+		weight= theWeight;
+	}
+	
+	//Setters and Getters
+	public String getName(){
+		return name;
+	}
+	public void setName(String name){
+		this.name= name;
+	}
+	public String getTypeOfPet(){
+		return typeOfPet;
+	}
+	public void setTypeOfPet(String typeOfPet){
+		this.typeOfPet= typeOfPet;
+	}
+	public int getAge(){
+		return age;
+	}
+	public void setAge(int age){
+		this.age=age;
+	}
+	public double getWeight(){
+		return weight;
+	}
+	public void setWeight(double weight){
+		this.weight=weight;
+	}
+}
